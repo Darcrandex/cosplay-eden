6a6b7666d906a3c01109f8d9573099618c43d6ae
darcrand <darcrandex@gmail.com>
2025-11-20 14:59:27 +0800

    🎉 init(cli): ok

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+
+      <img
+        src="https://cosplaytele.com/wp-content/uploads/2022/08/Nekokoyoshi-cosplay-Lux-League-of-Legends-9_result.webp"
+        alt="cosplay-fk"
+        width={128}
+        height={128}
+      />
+    </div>
+  );
+}
