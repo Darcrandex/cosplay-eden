f5e24eea636c0223cc3cbcc9f89566e8c1f38d45
darcrand <darcrandex@gmail.com>
2025-11-25 21:23:33 +0800

    🌈 style(app): 分页样式修改

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -20,7 +20,7 @@ export default function Pagination(props: {
       nextLabel={<ChevronRight className="h-4 w-4 transition-colors" />}
       breakLabel={<Ellipsis className="h-4 w-4 transition-colors" />}
       breakLinkClassName="flex items-center justify-center w-8 text-center py-1 text-gray-500 hover:text-pink-600 cursor-pointer"
-      pageLinkClassName="block min-w-8 text-center py-1 rounded-md truncate shrink-0 cursor-pointer text-gray-800 hover:text-pink-600 transition-colors hover:bg-pink-200"
+      pageLinkClassName="block min-w-8 text-center py-1 px-2 rounded-md truncate shrink-0 cursor-pointer text-gray-800 hover:text-pink-600 transition-colors hover:bg-pink-200"
       activeLinkClassName="bg-pink-600 text-white pointer-events-none"
       previousLinkClassName="flex items-center justify-center text-center w-8 h-8 text-gray-500 hover:text-pink-600 hover:bg-pink-200 cursor-pointer rounded-md"
       nextLinkClassName="flex items-center justify-center text-center w-8 h-8 text-gray-500 hover:text-pink-600 hover:bg-pink-200 cursor-pointer rounded-md"
